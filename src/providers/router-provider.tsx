8ba7c2c6a1a6f8e53a01e3458efa98092952ec4e
Amin Ahmady <aminahmadydeveloper@gmail.com>
2025-10-03 04:44:34 -0700

    chore: project initilized

--- src/providers/router-provider.tsx
+++ src/providers/router-provider.tsx
@@ -8,26 +8,29 @@ const HomePage = lazy(() => import('@/pages/home/home-page'));
 const AboutPage = lazy(() => import('@/pages/about/about-page'));
 const MoviePage = lazy(() => import('@/pages/movie/movie-page'));
 
-const router = createBrowserRouter([
-  {
-    children: [
-      {
-        element: <HomePage />,
-        index: true,
-      },
-      {
-        element: <AboutPage />,
-        path: 'about',
-      },
-      {
-        element: <MoviePage />,
-        path: 'movie/:movieId',
-      },
-    ],
-    element: <MainLayout />,
-    path: '/',
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      children: [
+        {
+          element: <HomePage />,
+          index: true,
+        },
+        {
+          element: <AboutPage />,
+          path: 'about',
+        },
+        {
+          element: <MoviePage />,
+          path: 'movie/:movieId',
+        },
+      ],
+      element: <MainLayout />,
+      path: '/',
+    },
+  ],
+  { basename: import.meta.env.DEV ? '/' : '/open-movie' },
+);
 
 export const RouterProvider: FC = () => {
   return <RouterProviderPrimitive router={router} />;
